9e1b6c5e40e14da565c801ade96b04821b9ec905
guptha <ratnajiguptha819@gmail.com>
2022-10-10 12:41:04 +0530

    1,2,3 steps are done

---
+++ src/main/java/com/guptha/jpa/hibernate/JpaHiberanteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.guptha.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHiberanteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHiberanteDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guptha/jpa/hibernate/JpaHiberanteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guptha.jpa.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHiberanteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
